617388500352e6f68a04f2da2057f9e2ef4297da
timse201 <timse201@users.noreply.github.com>
2026-09-25 22:44:17 +0200

    ca_valencia - add country_code and iso_3

--- locales/locales.php
+++ locales/locales.php
@@ -619,6 +619,8 @@ class GP_Locales {
 		$ca_valencia->native_name = 'Català (Valencià)';
 		$ca_valencia->lang_code_iso_639_1 = 'ca';
 		$ca_valencia->lang_code_iso_639_2 = 'cat';
+		$ca_valencia->lang_code_iso_639_3 = 'cat';
+		$ca_valencia->country_code = 'es';
 		$ca_valencia->wp_locale = 'ca_valencia';
 		$ca_valencia->slug = 'ca-val';
 		$ca_valencia->google_code = 'ca';
